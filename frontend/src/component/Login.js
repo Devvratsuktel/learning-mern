a4a9772ff7af28c1fa56374c3c8596dfc91d33e3
devvrat <devvratsuktel@gmail.com>
2024-02-02 23:44:38 +0530

    login

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,7 +1,36 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React,{useState} from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const navigate=useNavigate();
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+
+  const loginUser=async(e)=>{
+    e.preventDefault();
+    const res=await fetch("/signin",{
+      method:"POST",
+      headers:{
+        "content-Type":"application/json"
+      },
+      body:JSON.stringify({
+          email,password
+      })
+    });
+    const data=await res.json();
+    if(data.status===422||!data){
+      window.alert("Invalid credentials");
+      console.log("Invalid credentials");
+    }
+    else{
+      window.alert(" Registration sucessfull");
+      console.log(" Registration sucessfull");
+      navigate("/", { replace: true });
+    }
+
+  }
+
+
   return (
     <>
       <section>
@@ -13,16 +42,16 @@ const Login = () => {
           </div>
           <div class="row clearfix">
             <div class="">
-              <form>
+              <form method='POST'>
 
                 <div className="input_field"> <span><i aria-hidden="true" className="fa fa-user"></i></span>
-                      <input type="email" name="email" id="email" placeholder="email" required />
+                      <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} name="email" id="email" placeholder="email" required />
                 </div>
                 
                 <div className="input_field"> <span><i aria-hidden="true" className="fa fa-lock"></i></span>
-                  <input type="password" name="password"  id="password" placeholder="Password" required />
+                  <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} id="password" placeholder="Password" required />
                 </div>
-                <input className="butttn btn-success" type="submit" value="Login" id="Signup" />
+                <input className="butttn btn-success" type="submit" onClick={loginUser} value="Login" id="Signup" />
              
                </form> 
 
